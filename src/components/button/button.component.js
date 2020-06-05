e3892bff6feb19d0fb81ca517bda794ccf5ed87f
Safwen Daghsen <safouman@gmail.com>
2020-06-05 20:02:40 +0100

    added link to fb group

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -1,6 +1,13 @@
 import React from "react"
 import "./button.styles.scss"
 
-const Button = ({ btn_text }) => <div className="cta-btn">{btn_text}</div>
+const Button = ({ btn_text }) => (
+  <a
+    href="https://www.facebook.com/groups/184268029569784/"
+    className="cta-btn"
+  >
+    {btn_text}
+  </a>
+)
 
 export default Button
