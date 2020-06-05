dd2cce3a53ebe0567475c0511597357fd445c14c
Safwen Daghsen <safouman@gmail.com>
2020-06-05 18:44:22 +0100

    cleanign code

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,6 @@ module.exports = {
         name: `green-yoga-join`,
         short_name: `green-yoga`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
