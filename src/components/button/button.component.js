405ca13f92edc6338b639f2a8dc48548e18b7076
Safwen Daghsen <safwen.daghsen@gmail.com>
2020-07-08 16:30:24 +0100

    fixing the mobile side

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -2,12 +2,13 @@ import React from "react"
 import "./button.styles.scss"
 
 const Button = ({ btn_text }) => (
-  <a
-    href="https://www.facebook.com/groups/184268029569784/"
+  <button
+    
     className="cta-btn"
+    onClick={( )=>(window.location.href="https://www.facebook.com/groups/184268029569784/")}
   >
     {btn_text}
-  </a>
+  </button>
 )
 
 export default Button
